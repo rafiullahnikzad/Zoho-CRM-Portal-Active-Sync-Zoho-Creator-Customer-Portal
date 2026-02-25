cae0b9b7f7140375eb0d71bada0800b86a0c0b1b
Rafiullah <58433250+rafiullahnikzad@users.noreply.github.com>
2026-02-26 03:44:36 +0500

    Update Send_Portal_Invitation_to_Customers.js

--- Send_Portal_Invitation_to_Customers.js
+++ Send_Portal_Invitation_to_Customers.js
@@ -38,3 +38,18 @@ response = thisapp.portal.assignUserInProfile(input.Email, "Client");
 // NOTE: Profile name "Client" must match exactly as configured
 // in your Creator app under Settings > Portal > Permissions
 // ============================================================
+
+//
+//in Zoho creator create a function and put below code
+
+void appSharing.shareToUser(string userEmail)
+{
+	sharingResponse = thisapp.permissions.assignUserInProfile(userEmail,"User Contact");
+	sendmail
+	[
+		from :zoho.adminuserid
+		to :userEmail
+		subject :"Invitation to access '" + zoho.appname + "' "
+		message :"<div style='width:100%;display:inline-block;box-sizing:border-box;border:1px solid #ddd;'><div style='padding:2%;text-align:center;'>You've been invited to use an application built on Zoho Creator by \"" + zoho.adminuserid + "\".</div><div style='background: #efefef; padding: 20.0px;margin: 20.0px 0 10.0px;text-align:center;'><h2>" + zoho.appname.toUpperCase() + "</h2><div style='margin: 20.0px 0 10.0px;'><a style='padding:10px;background-color: rgb(62,149,205);text-decoration: none;color: rgb(255,255,255);font-size: 14.0px;' href='https://creator.zoho.com" + zoho.appuri + "' target='_blank'> Go to application </a></div></div></div>"
+	]
+}
